c04b65a3bb02fc8a6dab35dfbaae343acf613c13
Than Htut Oo <thanhtutoo.dev@gmail.com>
2021-05-11 22:04:24 +0800

    added more logs and push docker to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,12 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
-
+dockerpath=thanhtutooai/devops-project4
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
-
+docker login
+docker image tag devops-project4:latest $dockerpath
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
