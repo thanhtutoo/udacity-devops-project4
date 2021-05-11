9b53d213e1c0b639ad2d07980c42ccad9f7f73ad
Than Htut Oo <thanhtutoo.dev@gmail.com>
2021-05-11 22:14:17 +0800

    task 6 finish

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,13 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=thanhtutooai/devops-project4
 # Step 2
 # Run the Docker Hub container with kubernetes
-
-
+kubectl run devops-project4 --image=$dockerpath --port=80
 # Step 3:
 # List kubernetes pods
-
+kubectl get pods
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward devops-project4 8080:80
